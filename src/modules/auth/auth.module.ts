aa823996f7078764e6cfe9defc1a396fd4183229
Dev-Soares <bernardo.soares30@outlook.com>
2026-02-13 12:15:24 -0300

    fix: auth circular dependency fixed

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { HashModule } from '../../utils/hash/hash.module';
 
 @Module({
   providers: [AuthService],
   exports: [AuthService],
-  imports: [UserModule, JwtModule],
+  imports: [UserModule, JwtModule, HashModule],
 })
 export class AuthModule {}
--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { DatabaseModule } from '../database/database.module';
-import { AuthModule } from '../auth/auth.module';
 import { HashModule } from 'src/utils/hash/hash.module';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [DatabaseModule, AuthModule, HashModule],
+  imports: [DatabaseModule, HashModule],
   exports: [UserService],
 })
 export class UserModule {}
