361891c1bd6514e81e5ff99c4b5cd75aa40a98bd
Dev-Soares <bernardo.soares30@outlook.com>
2026-02-15 14:10:12 -0300

    feat: implemented swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const PORT = process.env.PORT || 3000
 
@@ -8,6 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SecurityOn API')
+    .setDescription('SecurityOn API documentation')
+    .setVersion('0.1')
+    .addTag('securityon')
+    .build();
   
   await app.listen(PORT);
   console.log(`API running on port ${PORT}`)
